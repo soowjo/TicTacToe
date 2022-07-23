62def6b225530ebbb61467ad1e9614525c9a0ee9
Soo <soowjo@gmail.com>
2022-07-23 15:59:09 +0900

    Revise js draft

--- script.js
+++ script.js
@@ -1,49 +1,103 @@
+'use strict'
+
 const Player = (entry) => {
     return {entry};
 };
 
 const gameBoard = (() => {
-    const rc11 = document.querySelector('#rc11');
-    const rc12 = document.querySelector('#rc12');
-    const rc13 = document.querySelector('#rc13');
-    const rc21 = document.querySelector('#rc21');
-    const rc22 = document.querySelector('#rc22');
-    const rc23 = document.querySelector('#rc23');
-    const rc31 = document.querySelector('#rc31');
-    const rc32 = document.querySelector('#rc32');
-    const rc33 = document.querySelector('#rc33');
-    let arr = [[rc11,rc12,rc13],[rc21,rc22,rc23],[rc31,rc32,rc33]];
-
-    const playerOne = Player([]);
-    const playerTwo = Player([]);
-    const bingo = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-    for (let i=0; i<arr.length; i++) {
-        for (let j=0; j<arr[i].length; j++) {
-            if (arr[i][j] ==='one') {
-                playerOne.entry.push(i*3+j+1);
-                playerOne.entry.sort();
-                if (bingo.includes(playerOne.entry)) {
-                    return 'One is winner!';
+    let arr = Array(9);
+    const playerO = Player([]);
+    const playerX = Player([]);
+    const bingo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    let turn = true;
+    let gameActive = true;
+    const checkBingo = function () {
+        for (let i=0; i<9; i++)
+            if (playerO.entry.length >= 3) {
+                playerO.sort();
+                if (bingo.includes(playerO.entry)) {
+                    gameActive = false;
+                    return 'O';
                 }
             }
-            else if (arr[i][j] ==='two') {
-                playerTwo.entry.push(i*3+j+1);
-                playerTwo.entry.sort();
-                if (bingo.includes(playerTwo.entry)) {
-                    return "Two is winner!";
+            else if (playerX.entry.length >= 3) {
+                playerX.sort();
+                if (bingo.includes(playerX.entry)) {
+                    gameActive = false;
+                    return 'X';
                 }
+            }
+            else if (i == 8) {
+                return 'draw'
+            }
+    }
+    
+    /* for (let i=0; i<arr.length; i++) {
+        if (arr[i] ==='one') {
+            playerOne.entry.push(i);
+            playerOne.entry.sort();
+            if (bingo.includes(playerOne.entry)) {
+                gameActive = false;
+                return 'One is winner!';
             }
         }
-    }
+        else if (arr[i] ==='two') {
+            playerTwo.entry.push(i);
+            playerTwo.entry.sort();
+            if (bingo.includes(playerTwo.entry)) {
+                gameActive = false;
+                return "Two is winner!";
+            }
+        }
+    } */
 })();
 
-displayController = (() => {
-    const playerOne = Player([]);
-    const playerTwo = Player([]);
-    const cells = document.querySelectorAll('.cells')
-    cells.forEach(i => i.addEventListener('click', clickingCell))
-    function clickingCell
-
-    /* write a while statement to swtich between players */
-
+const displayController = (() => {
+    const table = document.querySelector('#table');
+    const footer = document.querySelector('footer')
+    for (let i=0; i<arr.length; i++) {
+        const cell = document.createElement('div');
+        cell['id'] = 'cell'+i;
+        cell.addEventListener('click', () => {
+            if (gameBoard.turn) {
+                cell.textContent = 'O';
+                gameBoard.arr[i] = 'O';
+                playerO.push(i);
+                if (gameBoard.checkBingo() == 'O') {
+                    footer.textContent = 'O is winner';
+                    return;
+                };
+                else if (gameBoard.checkBingo() == 'draw') {
+                    footer.textContent = 'The result is draw';
+                    return;
+                };
+            }
+            else if (!gameBoard.turn) {
+                cell.textContent = 'X';
+                gameBoard.arr[i] = 'X';
+                playerX.push(i);
+                gameBoard.checkBingo();
+                if (gameBoard.checkBingo() == 'O') {
+                    footer.textContent = 'O is winner';
+                    return;
+                };
+                else if (gameBoard.checkBingo() == 'draw') {
+                    footer.textContent = 'The result is draw';
+                    return;
+                };
+            }
+        });
+        table.appendChild(cell);
+/*         `const c${i} = document.querySelector('#c${i}')`;
+        `c${i}.addEventListner('click', () => {
+            if (turn) {
+                c${i}.textContent = 'one';
+                gameBoard.playerOne.append(${i});
+            }
+            else if (!turn) {
+                c${i}.textContent = 'two';
+                gameBoard.playerTwo.append(${i});
+            }
+        })`; */
+    }
 })();
